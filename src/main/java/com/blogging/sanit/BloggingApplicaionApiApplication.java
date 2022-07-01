ccf01f76db6dbf35ba25066c49a17379baca1196
sanit7722 <sanit7722@gmail.com>
2022-07-01 20:36:10 +0530

    Initial Commit

---
+++ src/main/java/com/blogging/sanit/BloggingApplicaionApiApplication.java
@@ -0,0 +1,13 @@
+package com.blogging.sanit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggingApplicaionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggingApplicaionApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blogging/sanit/BloggingApplicaionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogging.sanit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingApplicaionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
