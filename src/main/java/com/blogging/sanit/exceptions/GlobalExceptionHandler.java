da15b58285cddadf8a90ebd84b98203be9fb70a2
sanit7722 <sanit7722@gmail.com>
2022-07-02 20:39:30 +0530

    Handled user API's Exceptions

---
+++ src/main/java/com/blogging/sanit/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.blogging.sanit.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.blogging.sanit.payloads.ApiResponse;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<ApiResponse> resourceNotFoundExceptionHandler(
+			ResourceNotFoundException  ex){
+		String message=ex.getMessage();
+		ApiResponse apiResponse=new ApiResponse(message, false);
+		return new ResponseEntity<ApiResponse>(apiResponse,HttpStatus.NOT_FOUND);
+	}
+	
+}
