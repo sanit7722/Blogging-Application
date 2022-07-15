4d48aca25943512203dd38147107c116720d94c5
sanit7722 <sanit7722@gmail.com>
2022-07-15 20:56:25 +0530

    Added jwt methods

--- src/main/java/com/blogging/sanit/security/JwtTokenHelper.java
+++ src/main/java/com/blogging/sanit/security/JwtTokenHelper.java
@@ -1,8 +1,54 @@
 package com.blogging.sanit.security;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import javax.xml.crypto.Data;
+
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 
 @Component
 public class JwtTokenHelper {
 
+	public static final long JWT_TOKEN_VALIDITY=5*60*60;
+	
+	private String secret="jwtTokenKey";
+	
+	public String getUsernameFromToken(String token) {
+		return getClaimFromToken(token,Claims::getSubject);
+		
+	}
+	public Date getExpirationDataFromToken(String token) {
+		return getClaimFromToken(token, Claims::getExpiration);
+	}
+	
+	public<T>T getClaimFromToken(String token, Function<Claims,T> claimsResolver){
+		final Claims claims=getAllClaimsFromToken(token);
+		return claimsResolver.apply(claims);
+	}
+	
+	private Claims getAllClaimsFromToken(String token) {
+		return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+	}
+	
+	private Boolean isTokenExpired(String token) {
+		final Date expiration=getExpirationDataFromToken(token);
+		return expiration.before(new Date());
+	}
+	
+	public String generateToken(UserDetails userDetails) {
+		Map<String, Object>claims=new HashMap<>();
+		return doGeneratedToken(claims, userDetails.getUsername());
+	}
+	public Boolean validateToken(String token, UserDetails userDetails) {
+		final String username=getUsernameFromToken(token);
+		return(username.equals(userDetails.getUsername())&& !isTokenExpired(token));
+	}
+	
 }
